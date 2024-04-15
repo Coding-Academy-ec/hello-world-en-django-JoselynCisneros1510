457b730a15c0f04833498b633ae2595507c8e727
JoselynCisneros1510 <joselyn@corporaciondfl.com>
2024-04-14 21:21:39 -0500

    helloWorld

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import hello_world
 
 urlpatterns = [
-    # define la ruta para la vista hello_world.
+    # Define la ruta para la vista hello_world
+    path('', hello_world)
 ]
